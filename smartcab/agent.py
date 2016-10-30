e70f3cfae72fd7a843b1ca9837f2db40c859e0ee
pushpendra pratap <pratappushpendra1993@gmail.com>
2016-10-30 20:58:03 +0530

    using random action

---
+++ smartcab/agent.py
@@ -0,0 +1,79 @@
+import random
+from environment import Agent, Environment
+from planner import RoutePlanner
+from simulator import Simulator
+
+class LearningAgent(Agent):
+    """An agent that learns to drive in the smartcab world."""
+
+    def __init__(self, env):
+        super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
+        self.color = 'red'  # override color
+        self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
+
+        # TODO: Initialize any additional variables here
+        self.successes = []
+
+    def reset(self, destination=None):
+        self.planner.route_to(destination)
+
+        # TODO: Prepare for a new trip; reset any variables here, if required
+        self.successes.append(False)
+
+    def update(self, t):
+        # Gather inputs
+        self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
+        inputs = self.env.sense(self)
+        deadline = self.env.get_deadline(self)
+
+        # TODO: Update state
+        
+        # TODO: Select action according to your policy
+        action = random.choice([None, 'forward', 'left', 'right'])    # For Question 1
+
+        # Execute action and get reward
+        reward = self.env.act(self, action)
+
+        # TODO: Learn policy based on state, action, reward
+
+        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+
+        # Pushpendra did this for model evaluation purpose
+        my_location = self.env.agent_states[self]['location']
+        my_destination = self.env.agent_states[self]['destination']
+
+        if (my_location == my_destination):       # Just for measuring performance, corresponding to all the Trials
+            self.successes[-1] = True
+
+
+def run():
+    """Run the agent for a finite number of trials."""
+
+    # Set up environment and agent
+    e = Environment()  # create environment (also adds some dummy traffic)
+    a = e.create_agent(LearningAgent)  # create agent
+    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
+    # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
+
+    # Now simulate it
+    sim = Simulator(e, update_delay=0.0000005, display=False)  # create simulator (uses pygame when display=True, if available)
+    # NOTE: To speed up simulation, reduce update_delay and/or set display=False
+
+    sim.run(n_trials=100)  # run for a specified number of trials
+    # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
+
+    return a.successes
+
+
+if __name__ == '__main__':
+    out = open('random_action_evaluation.csv','w')
+    column = ('success ratio when using random action')
+    out.write('%s' %column)
+    out.write('\n')
+    out.close()
+    out = open('random_action_evaluation.csv','a')
+    for i in range(20):
+        success_list = run()
+        success_ratio = success_list.count(True) / (1.0 * len(success_list))
+        out.write('%f' %success_ratio)
+        out.write('\n')
